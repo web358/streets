b1da6ce025dc06112238e9ea045c6182fa2a694d
web358 <iva.a.atanasova@abv.bg>
2025-04-26 09:01:22 +0300

    Update helper.js

--- helper.js
+++ helper.js
@@ -38,3 +38,24 @@ let btn = document.querySelector('.closeBtn')
 btn.addEventListener('click', () => {
 document.querySelector('.tableDiv').style.display = 'none'
 })
+
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.getElementsByClassName('image__container');
+
+    // Hide all images
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+
+    // Show current image
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].classList.add('active');
+
+    setTimeout(showSlides, 1500);
+}
